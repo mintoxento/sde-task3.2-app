d6b080243f8f22dbc95bd249a0ae6ac9540225b3
JuneH <168982270+mintoxento@users.noreply.github.com>
2026-04-10 13:03:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ SECRET_MSG = os.environ.get("SECRET_MSG", "No secret set")
 
 @app.route("/")
 def home():
-    return f"<h1>Flask App on Render</h1><p>Secret: {SECRET_MSG}</p>"
+    return f"<h1>Flask App on Render: Test-Auto-Deploy</h1><p>Secret: {SECRET_MSG}</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
